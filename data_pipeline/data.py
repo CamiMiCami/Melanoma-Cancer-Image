3685197716cec1ce6e71b977533df79be96d9059
st180457 <st180457@stud.uni-stuttgart.de>
2024-03-08 16:11:33 +0100

    111

---
+++ data_pipeline/data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import json
+import numpy as np
+import tensorflow as tf
+
+with open(r'C:\Users\shuai\Desktop\sarcasm_detection\Sarcasm.json', 'r') as f:
+  data = json.load(f)
